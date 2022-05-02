c6a21f8dcf38f3acd2380081614f360ffaaa541d
AaronDewes <aaron.dewes@protonmail.com>
2022-05-02 09:33:06 +0000

    Add WIP version

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,52 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
-});
+import type __Vue from "vue";
+import type { Store } from "vuex";
+
+type GameStore = {
+  assetsCDN: string;
+  blackScreen: {
+    solved: boolean;
+    val: string | null;
+  };
+  dns: Array<
+    | {
+        file: string;
+        url: string;
+        game?: boolean;
+      }
+    | {
+        redirect: string;
+        url: string;
+      }
+  >;
+  mineleaped: boolean;
+  runhacx: {
+    cloned: boolean;
+    installed: boolean;
+  };
+};
+
+function sleep(milliseconds: number) {
+ return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+window.onload = async () => {
+  type _Vue = __Vue & {
+    $store: Store<GameStore>;
+  };
+  await sleep(30000);
+  console.log(document.querySelectorAll("*"));
+  // @ts-expect-error
+  const Vue = Array.from(document.querySelectorAll("*")).find((e) => e.__vue__)
+    // @ts-expect-error
+    .__vue__ as _Vue;
+
+  Vue.$root.$emit("clippy-speak", "CodeBreaker extension activated!");
+
+  const $store = Vue.$store;
+
+  // @ts-expect-error
+  window.breakTheCode = {
+    vue: Vue,
+    isModded: true,
+  };
+};
